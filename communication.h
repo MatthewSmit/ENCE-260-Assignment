c802cb77d1b737c58af68a357ac3daafa66d1fe0
Matthew Smit <mjs351@uclive.ac.nz>
2016-10-05 15:14:51 +1300

    added communication.h

---
+++ communication.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef enum {
+	RESULT_NONE,
+	RESULT_PAPER,
+	RESULT_SCISSORS,
+	RESULT_ROCK,
+	RESULT_CONFIRMED,
+} Result;
+
+#define PAPER '='
+#define SCISSORS '>'
+#define ROCK '<'
+
+void send_type(char type);
+void send_confirmation(void);
+Result get_result(void);
